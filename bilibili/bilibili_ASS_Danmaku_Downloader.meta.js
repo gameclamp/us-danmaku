fb20b5c9155f2c3178e99aa46063bc9c4aaf5df0
9尾雪狐 <gameclamp@gmail.com>
2017-06-28 23:45:24 +0800

    Update bilibili_ASS_Danmaku_Downloader.meta.js

--- bilibili/bilibili_ASS_Danmaku_Downloader.meta.js
+++ bilibili/bilibili_ASS_Danmaku_Downloader.meta.js
@@ -1,19 +1,21 @@
 // ==UserScript==
 // @name        bilibili ASS Danmaku Downloader
-// @namespace   https://github.com/tiansh
+// @namespace   https://github.com/gameclamp
 // @description 以 ASS 格式下载 bilibili 的弹幕
-// @include     http://www.bilibili.com/video/av*
-// @include     http://www.bilibili.tv/video/av*
-// @include     http://bilibili.kankanews.com/video/av*
-// @updateURL   https://tiansh.github.io/us-danmaku/bilibili/bilibili_ASS_Danmaku_Downloader.meta.js
-// @downloadURL https://tiansh.github.io/us-danmaku/bilibili/bilibili_ASS_Danmaku_Downloader.user.js
-// @version     1.11
+// @include     https://www.bilibili.com/video/av*
+// @include     https://bangumi.bilibili.com/movie/*
+// @include     https://bangumi.bilibili.com/anime/*
+// @updateURL   https://github.com/gameclamp/us-danmaku/raw/master/bilibili/bilibili_ASS_Danmaku_Downloader.meta.js
+// @downloadURL https://github.com/gameclamp/us-danmaku/raw/master/bilibili/bilibili_ASS_Danmaku_Downloader.user.js
+// @version     1.12
 // @grant       GM_addStyle
 // @grant       GM_xmlhttpRequest
 // @run-at      document-start
 // @author      田生
+// @fixer       Gameclamp
 // @copyright   2014+, 田生
 // @license     Mozilla Public License 2.0; http://www.mozilla.org/MPL/2.0/
 // @license     CC Attribution-ShareAlike 4.0 International; http://creativecommons.org/licenses/by-sa/4.0/
+// @connect-src comment.bilibili.com
+// @connect-src interface.bilibili.com
 // ==/UserScript==
-
